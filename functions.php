4fdaa8e47d8446a3f8d61055451787d0b9a49ffb
Florian Brinkmann <kontakt@florianbrinkmann.de>
2016-09-28 18:21:02 +0200

    Added content.php and template tags for title;content;meta data

--- functions.php
+++ functions.php
@@ -98,3 +98,118 @@ function schlicht_get_custom_logo() {
 		return get_custom_logo();
 	}
 }
+
+/**
+ * Displays the title of a post
+ *
+ * @param $heading , $link
+ *
+ * @return void
+ */
+function schlicht_the_title( $heading, $link = true ) {
+	if ( $link ) {
+		the_title( sprintf(
+			'<%1$s class="entry-title"><a href="%2$s" rel="bookmark">',
+			$heading, esc_url( get_permalink() )
+		), sprintf( '</a></%s>', $heading ) );
+	} else {
+		the_title( sprintf(
+			'<%1$s class="entry-title">',
+			$heading, esc_url( get_permalink() )
+		), sprintf( '</%s>', $heading ) );
+	}
+}
+
+/**
+ * Displays the_content with a more accessible more tag
+ *
+ * @return void
+ */
+function schlicht_the_content() {
+	/* translators: visible text for the more tag */
+	the_content(
+		sprintf(
+			'<span aria-hidden="true">%1s</span><span class="screen-reader-text">%2s</span>',
+			__( 'Continue reading', 'schlicht' ),
+			sprintf( /* translators: continue reading text for screen reader users. s=post title */
+				__( 'Continue reading %s', 'schlicht' ),
+				the_title( '', '', false )
+			)
+		)
+	);
+}
+
+/**
+ * Displays footer meta for a post
+ *
+ * @return void
+ */
+function schlicht_the_post_meta() {
+	if ( get_the_category() ) { ?>
+		<p class="entry-footer-block categories"><span class="entry-footer-block-label">
+				<?php /* translators: Label for category list in entry footer. s=categories */
+				printf( _n(
+					'Category',
+					'Categories',
+					count( get_the_category() ),
+					'schlicht'
+				) )
+				?><span class="screen-reader-text">:</span></span>
+			<?php /* translators: term delimiter */
+			echo get_the_category_list( __( ', ', 'schlicht' ) ); ?>
+		</p>
+	<?php }
+	if ( get_the_tags() ) { ?>
+		<p class="entry-footer-block tags"><span class="entry-footer-block-label">
+				<?php /* translators: Label for tags list in entry footer. */
+				printf( _n(
+					'Tag',
+					'Tags',
+					count( get_the_tags() ),
+					'schlicht'
+				) )
+				?><span class="screen-reader-text">:</span></span>
+			<?php /* translators: term delimiter */
+			echo get_the_tag_list( '', __( ', ', 'hannover' ) ); ?>
+		</p>
+	<?php }
+	$comments_by_type = schlicht_get_comments_by_type();
+	if ( $comments_by_type['comment'] ) {
+		$comment_number = count( $comments_by_type['comment'] ); ?>
+		<p class="entry-footer-block comments"><span class="entry-footer-block-label">
+				<?php /* translators: Label for comment number in entry footer. */
+				_e( 'Comments', 'schlicht' );
+				?><span class="screen-reader-text">:</span></span>
+			<?php /* translators: term delimiter */
+			echo number_format_i18n( $comment_number ); ?>
+		</p>
+	<?php }
+	if ( $comments_by_type['pings'] ) {
+		$trackback_number = count( $comments_by_type['pings'] ); ?>
+		<p class="entry-footer-block trackbacks"><span class="entry-footer-block-label">
+				<?php /* translators: Label for trackback number in entry footer. */
+				_e( 'Trackbacks', 'schlicht' );
+				?><span class="screen-reader-text">:</span></span>
+			<?php /* translators: term delimiter */
+			echo number_format_i18n( $trackback_number ); ?>
+		</p>
+	<?php };
+}
+
+/**
+ * Gets the comments seperated by type
+ *
+ * @return array
+ */
+function schlicht_get_comments_by_type() {
+	$comment_args     = array(
+		'order'   => 'ASC',
+		'orderby' => 'comment_date_gmt',
+		'status'  => 'approve',
+		'post_id' => get_the_ID(),
+	);
+	$comments         = get_comments( $comment_args );
+	$comments_by_type = separate_comments( $comments );
+
+	return $comments_by_type;
+}
---
+++ template-parts/content.php
@@ -0,0 +1,21 @@
+<article <?php post_class( 'clearfix' ); ?>>
+	<header class="entry-header">
+		<?php if ( is_sticky() ) {
+			/* translators: String displayed above headline of sticky post(s) */ ?>
+			<span class="featured"><?php _e( 'Featured', 'schlicht' ); ?></span>
+		<?php }
+		schlicht_the_title( 'h2' ) ?>
+		<div class="author-date">
+			<p class="author"><?php
+				/* translators: s=author name */
+				printf( __( 'by %s', 'schlicht' ), get_the_author() ) ?></p>
+			<p class="date"><?php echo get_the_date(); ?></p>
+		</div>
+	</header>
+	<div class="entry-content">
+		<?php schlicht_the_content(); ?>
+	</div>
+	<footer class="entry-footer">
+		<?php schlicht_the_post_meta(); ?>
+	</footer>
+</article>
