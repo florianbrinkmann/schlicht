8b48540758b0f855e05b5f9586dbbcabccadc281
Florian Brinkmann <kontakt@florianbrinkmann.de>
2016-09-28 15:21:06 +0200

    Added index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Main Template file
+ */
+get_header(); ?>
+	<main>
+		<?php if ( have_posts() ) {
+			if ( is_home() && ! is_front_page() ) { ?>
+				<header>
+					<h1 class="screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+			<?php }
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/content', get_post_format() );
+			}
+		} else {
+			get_template_part( 'template-parts/content', 'none' );
+		}
+		the_posts_pagination( array( 'type' => 'list' ) ); ?>
+	</main>
+<?php get_sidebar();
+get_footer();
