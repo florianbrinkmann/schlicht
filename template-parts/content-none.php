c8fa1f5308c405896151a209cf8ddd775f51821a
Florian Brinkmann <kontakt@florianbrinkmann.de>
2016-09-29 14:56:30 +0200

    added content-none.php

---
+++ template-parts/content-none.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template part if have_posts() is false
+ *
+ * @version 1.0
+ */
+?>
+<article class="post">
+	<p><?php _e( 'Sorry, no posts matched your criteria.', 'schlicht' ); ?></p>
+</article>
