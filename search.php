f979913ecba0d991b641214a89f732794c00b00c
Florian Brinkmann <kontakt@florianbrinkmann.de>
2016-10-02 11:12:44 +0200

    added search.php

---
+++ search.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template file for search result page
+ *
+ * @version 1.0
+ */
+get_header(); ?>
+	<main>
+		<?php if ( have_posts() ) { ?>
+			<header>
+				<h1 class="archive-title">
+					<?php printf( __( 'Search Results for: %s', 'schlicht' ), esc_html( get_search_query() ) ); ?>
+				</h1>
+			</header>
+			<?php while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/content', get_post_format() );
+			}
+		} else { ?>
+			<header>
+				<h1 class="archive-title">
+					<?php printf( __( 'Nothing found for: %s', 'schlicht' ), esc_html( get_search_query() ) ); ?>
+				</h1>
+			</header>
+			<?php get_search_form();
+		}
+		the_posts_pagination( array( 'type' => 'list' ) ); ?>
+	</main>
+<?php get_sidebar();
+get_footer();
