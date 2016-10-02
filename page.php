6aa4324d4e86184f775915da2f2e41a527500995
Florian Brinkmann <kontakt@florianbrinkmann.de>
2016-10-02 11:06:38 +0200

    added page.php and content-page.php

---
+++ page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template for displaying pages
+ *
+ * @version 1.0
+ */
+get_header(); ?>
+	<main>
+		<?php if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/content', 'page' );
+			}
+		} else {
+			get_template_part( 'template-parts/content', 'none' );
+		} ?>
+	</main>
+<?php get_sidebar();
+get_footer();
---
+++ template-parts/content-page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template for displaying content of pages
+ *
+ * @version 1.0
+ */
+?>
+<article <?php post_class( 'clearfix' ); ?>>
+	<header class="entry-header">
+		<?php schlicht_the_title( 'h1', false ) ?>
+	</header>
+	<div class="entry-content">
+		<?php the_post_thumbnail( 'large' );
+		schlicht_the_content();
+		wp_link_pages(); ?>
+	</div>
+</article>
