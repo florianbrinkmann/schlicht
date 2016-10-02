0d5e0736409b569b520b3f259ab3aebb82510863
Florian Brinkmann <kontakt@florianbrinkmann.de>
2016-10-02 11:04:09 +0200

    added archive.php

---
+++ archive.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template file for archives
+ *
+ * @version 1.0
+ */
+get_header(); ?>
+	<main>
+		<?php if ( have_posts() ) { ?>
+			<header>
+				<h1 class="archive-title"><?php the_archive_title(); ?></h1>
+				<?php $archive_description = get_the_archive_description();
+				if ( $archive_description != '' ) {
+					echo $archive_description;
+				} ?>
+			</header>
+			<?php while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/content', get_post_format() );
+			}
+		} else {
+			get_template_part( 'template-parts/content', 'none' );
+		}
+		the_posts_pagination( array( 'type' => 'list' ) ); ?>
+	</main>
+<?php get_sidebar();
+get_footer();
