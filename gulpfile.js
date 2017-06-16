05669d089bdaf32356c3732e1d892611516a76a7
Florian Brinkmann <mail@flobri.de>
2017-06-16 13:34:18 +0200

    Added Gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const flipper = require('gulp-css-flipper');
+
+gulp.task('css-rtl', function () {
+	return gulp.src(['css/*.css', '!css/*-rtl.css'])
+		.pipe(flipper())
+		.pipe(rename(
+			{suffix: "-rtl"}
+		))
+		.pipe(gulp.dest('css/'));
+});
+
+gulp.task('default', ['css-rtl']);
+
+gulp.task('production', ['css-rtl']);
