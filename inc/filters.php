8c06baf342916c284ee37e4f8010f76f999a5a95
Florian Brinkmann <mail@flobri.de>
2017-06-17 09:33:25 +0200

    filters.php docs

--- inc/filters.php
+++ inc/filters.php
@@ -7,8 +7,17 @@
  * @package Schlicht
  */
 
+/**
+ * Remove #more jump from read more link.
+ */
 add_filter( 'the_content_more_link', 'schlicht_remove_more_link_scroll' );
 
+/**
+ * Filters the body classes.
+ */
 add_filter( 'body_class', 'schlicht_body_classes' );
 
+/**
+ * Check for Schlicht updates.
+ */
 add_filter( 'pre_set_site_transient_update_themes', 'schlicht_theme_update' );
