ea8251396e508cdf818cfc66b2b08cb70a99aa67
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-07-24 16:05:07 +0200

    and again

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -2,12 +2,12 @@
 # @link https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
 set -e # Exit with nonzero exit code if anything fails
 
+SOURCE_BRANCH="master"
 TARGET_BRANCH="production"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
+    echo "Skipping deploy; just exit."
     exit 0
 fi
 
