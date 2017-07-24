52cab2883185513bb91d9597b7999d87a7aaf9b7
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-07-24 16:25:47 +0200

    Of course we need a merge…

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -22,6 +22,7 @@ SHA=`git rev-parse --verify HEAD`
 git clone $REPO ${SHA}
 cd ${SHA}
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git merge SOURCE_BRANCH
 
 # Run our compile script
 npm install --silent
