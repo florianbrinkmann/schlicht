86cfc02ce3ce83a71ef7abef1f652dedf1ca9c4c
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-07-24 14:23:16 +0200

    Next try for GH script

--- scripts/push.sh
+++ scripts/push.sh
@@ -13,7 +13,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  #git remote add origin https://${GH_TOKEN}@github.com/FlorianBrinkmann/schlicht.git > /dev/null 2>&1
+  #git remote add origin https://$GH_TOKEN@github.com/FlorianBrinkmann/schlicht.git > /dev/null 2>&1
   #git push --set-upstream production-branch
 }
 
