878740c728018c3589bffe336cf83205dd076778
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-07-24 14:16:12 +0200

    Next try for GH push

--- .travis/push.sh
+++ .travis/push.sh
@@ -13,8 +13,8 @@ commit_website_files() {
 }
 
 upload_files() {
-  #git remote add origin-pages https://${GH_TOKEN}@github.com/FlorianBrinkmann/schlicht.git > /dev/null 2>&1
-  #git push --quiet --set-upstream origin-pages gh-pages
+  #git remote add origin https://${GH_TOKEN}@github.com/FlorianBrinkmann/schlicht.git > /dev/null 2>&1
+  #git push --set-upstream production-branch
 }
 
 setup_git
