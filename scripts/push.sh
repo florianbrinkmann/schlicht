fbc1e68fae21f66589a791fb23fd14016ac9ac33
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-07-24 14:40:33 +0200

    And the next try

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,22 +1,62 @@
-#!/bin/sh
-# @link https://gist.github.com/willprice/e07efd73fb7f13f917ea
+#!/bin/bash
+# @link https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+set -e # Exit with nonzero exit code if anything fails
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
+SOURCE_BRANCH="master"
+TARGET_BRANCH="production"
 
-commit_website_files() {
-  git checkout -b production-branch
-  git add . --dry-run
-  #git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    doCompile
+    exit 0
+fi
 
-upload_files() {
-  #git remote add origin https://$GH_TOKEN@github.com/FlorianBrinkmann/schlicht.git > /dev/null 2>&1
-  #git push --set-upstream production-branch
-}
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
 
-setup_git
-commit_website_files
-upload_files
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone $REPO out
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Clean out existing contents
+rm -rf out/**/* || exit 0
+
+# Run our compile script
+npm install --silent
+npm install -g gulp-cli
+gulp production
+
+# Delete all files and directories we do not need in production
+rm -r .dpl
+rm -r .node_modules
+rm -r .gulpfile.js
+rm -r .gitignore
+rm -r .deploy_rsa.enc
+rm -r scripts
+rm -r package.json
+rm -r .travis.yml
+
+# Now let's go have some fun with the cloned repo
+cd out
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if git diff --quiet; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add -A .
+git commit -m "Deploy to production branch: ${SHA}"
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
