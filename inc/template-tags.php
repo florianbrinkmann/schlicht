d8b3bf5f823f8a1e4698ba2c7541b348ad194b8e
Florian Brinkmann <mail@flobri.de>
2019-01-01 17:51:46 +0100

    Fix escaping error

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -115,7 +115,7 @@ if ( ! function_exists( 'schlicht_the_post_meta' ) ) {
 				<?php
 				// Display the category list.
 				/* translators: term delimiter */
-				echo esc_html( get_the_category_list( __( ', ', 'schlicht' ) ) );
+				echo get_the_category_list( __( ', ', 'schlicht' ) );
 				?>
 			</p>
 			<?php
@@ -140,7 +140,7 @@ if ( ! function_exists( 'schlicht_the_post_meta' ) ) {
 				<?php
 				// Display the tag list.
 				/* translators: term delimiter */
-				echo esc_html( get_the_tag_list( '', __( ', ', 'schlicht' ) ) );
+				echo get_the_tag_list( '', __( ', ', 'schlicht' ) ); //phpcs:ignore
 				?>
 			</p>
 			<?php
